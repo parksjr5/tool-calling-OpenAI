2592d3bce9dc61dc328231b13154df33f421aadd
Jessica Parks <jparks@onemagnify.com>
2025-04-18 11:35:16 -0400

    add optional lat, long parameters to get_weather

--- weather.py
+++ weather.py
@@ -1,10 +1,10 @@
 import requests
 from creds import credentials
 
-def get_weather():
+def get_weather(lat="42.7336", long="-84.5539"):
     weather_api = credentials['weather_api']
 
-    url = f"https://api.tomorrow.io/v4/weather/realtime?location=42.7336,-84.5539&apikey={weather_api}"
+    url = f"https://api.tomorrow.io/v4/weather/realtime?location={lat},{long}&apikey={weather_api}"
 
     headers = {
         "accept": "application/json"
